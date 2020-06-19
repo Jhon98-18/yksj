23e605229ddc51fbc82d6729539ce3cec31c63f5
zhangzhan@yunksj.com <zhangzhan@yunksj.com>
2020-06-19 16:34:02 +0800

    git master A

--- src/TestMain.java
+++ src/TestMain.java
@@ -2,6 +2,7 @@ import java.lang.reflect.Constructor;
 
 public class TestMain {
     public static void main(String[] args) {
+        //git master A
 //git B
         Class<Long> longClass = Long.class;
         Constructor<?>[] constructors = longClass.getConstructors();
