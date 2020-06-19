b9eef90b1eb204bd6a98e92a8b154a167067802d
zhangzhan@yunksj.com <zhangzhan@yunksj.com>
2020-06-19 16:13:16 +0800

    git (还原测试)

--- src/TestMain.java
+++ src/TestMain.java
@@ -2,7 +2,6 @@ import java.lang.reflect.Constructor;
 
 public class TestMain {
     public static void main(String[] args) {
-        //master A
 
         Class<Long> longClass = Long.class;
         Constructor<?>[] constructors = longClass.getConstructors();
