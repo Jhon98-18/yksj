5f99ef5757c6c9817c8db60d0d1d4a4d2acf5e5a
zhangzhan@yunksj.com <zhangzhan@yunksj.com>
2020-06-22 09:30:56 +0800

    git 在devlop 分支rebasemaster

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,7 +3,7 @@ import java.lang.reflect.Constructor;
 public class TestMain {
     public static void main(String[] args) {
 //git  master a
-//git  b
+//git  devlop b
         Class<Long> longClass = Long.class;
         Constructor<?>[] constructors = longClass.getConstructors();
     }
