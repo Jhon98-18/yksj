c574ddff2fbe643a7aa3c65991d41af82235ea55
zhangzhan@yunksj.com <zhangzhan@yunksj.com>
2020-06-22 09:46:20 +0800

    git master测试push3

--- src/TestMain.java
+++ src/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
     public static void main(String[] args) {
 //git  master开发新功能 a
 //git  devlop b
+        //测试push3
         Class<Long> longClass = Long.class;
         Constructor<?>[] constructors = longClass.getConstructors();
     }
