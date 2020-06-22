e70e4f575773c8bcc278819aaebeb00aadcf4a23
zhangzhan@yunksj.com <zhangzhan@yunksj.com>
2020-06-22 09:44:42 +0800

    git 测试push1

--- src/TestMain.java
+++ src/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
     public static void main(String[] args) {
 //git  开发新功能 a
 //git  开发新功能 b
+        //测试push1
         Class<Long> longClass = Long.class;
         Constructor<?>[] constructors = longClass.getConstructors();
     }
