4a1005bdff8e5425dbf9add6f26d0aea4bda7a55
zhangzhan@yunksj.com <zhangzhan@yunksj.com>
2020-06-22 09:38:43 +0800

    git 开发新功能 a

--- src/TestMain.java
+++ src/TestMain.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Constructor;
 
 public class TestMain {
     public static void main(String[] args) {
-//git  master a
+//git  master开发新功能 a
 //git  devlop b
         Class<Long> longClass = Long.class;
         Constructor<?>[] constructors = longClass.getConstructors();
